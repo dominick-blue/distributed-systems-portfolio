7b482ffaa0813c727e38e96b7b286d57db21de89
Dominick Blue <dominick.blue7@gmail.com>
2024-10-19 15:16:25 -0700

    Implementing liquibase properties across the microservices

--- microservices-ecommerce-platform/user-service/src/test/java/com/loyalblue/ecommerce/user/repository/UserRepositoryTest.java
+++ microservices-ecommerce-platform/user-service/src/test/java/com/loyalblue/ecommerce/user/repository/UserRepositoryTest.java
@@ -1,11 +1,14 @@
 package com.loyalblue.ecommerce.user.repository;
 
 import com.loyalblue.ecommerce.user.model.User;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 import java.util.Optional;
 
